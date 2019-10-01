a30544cf8f044f6fb13ec312867e477bc44d142f
Slipchenko Alexey <55618990+Alexkvintin@users.noreply.github.com>
2019-10-01 14:08:56 +0300

    Update hw_3_1.py

--- lesson03/hw_3_1.py
+++ lesson03/hw_3_1.py
@@ -1,18 +1,18 @@
 a = int(input("введите цифру 1 для начала работы"))
 
 
-def _sum(n):
+def calc(n):
     print("количество символов в вашем числе =", len(n))
 
 
 while a == 1:
     b = int(input("введите целое, натуральное число: "))
-    if b <= 0:
+    if b == 0:
         print("Вы ввели число не соответствующее требованиям")
         continue
     elif b is int(b):
         b = str(b)
-        _sum(b)
+        calc(b)
     a = int(input("хотите продолжить ? (1 - да, 0 - нет)"))
     if a == 1:
         continue

c760a8ee63f59ed0f00c8b6de285215b93de26cd
Slipchenko Alexey <55618990+Alexkvintin@users.noreply.github.com>
2019-10-01 15:57:08 +0300

    Update hw_3_3.py

--- lesson03/hw_3_3.py
+++ lesson03/hw_3_3.py
@@ -1,1 +1,58 @@
+def circle(x):
+    y = int(x ** 2)
+    s = 3.14 * y
+    print("площадь круга равна:", s)
 
+
+def triangle(x, y, z):
+    import math
+    p = float((x + y + z) / 2)
+    s = (math.sqrt(p * (p - x) * (p - y) * (p - z)))
+    print("площадь треугольника равна:", s)
+
+
+def rectangle(x, y):
+    s = int(x * y)
+    print("Площадь прямоугольника равна", s)
+
+
+f = input("для начала работы програмы нажмите ENTER ")
+while f is not None:
+    a = input("""                     Площадь какой фигуры вы хотите вычеслить ?
+                                   Круг (1)
+                                   Треугольник (2)
+                                   Квадрат (3)
+                           Для выхода нажмите ENTER (4)""")
+    if a == "1" or a == "круг" or a == "circle":
+        b = float(input("Ввудите радиус круга:"))
+        circle(b)
+        k = int(input("для продолжения нажмите 1 для выхода 0: "))
+        if k == 1:
+            continue
+        elif k == 0:
+            break
+    elif a == "2" or a == "теугольник" or a == "triangle":
+        b = float(input("Введите размер первой стороны треугольника:"))
+        c = float(input("Введите размер второй стороны треугольника:"))
+        d = float(input("Введите размер третей стороны треугольника:"))
+        triangle(b, c, d)
+        k = int(input("для продолжения нажмите 1 для выхода 0: "))
+        if k == 1:
+            continue
+        elif k == 0:
+            break
+    elif a == "3" or a == "прямоугольник" or a == "rectangle":
+        d = float(input("Введите длину прямоугольника:"))
+        c = float(input("Введите ширину прямоугольника:"))
+        rectangle(d, c)
+        k = int(input("для продолжения нажмите 1 для выхода 0: "))
+        if k == 1:
+            continue
+        elif k == 0:
+            break
+    elif a == "" \
+              "" or a == "4":
+        a = 4
+        if a == 4:
+            break
+print("работа программы завершена")

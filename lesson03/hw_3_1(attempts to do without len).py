fb4dab0734030e9b2b57e0253cf255ad43a644b6
Slipchenko Alexey <55618990+Alexkvintin@users.noreply.github.com>
2019-10-01 17:21:00 +0300

    Add files via upload

---
+++ lesson03/hw_3_1(attempts to do without len).py
@@ -0,0 +1,23 @@
+import math
+a = int(input("введите цифру 1 для начала работы"))
+
+
+def calc(n):
+    sum(c != ' ' for c in n)
+
+
+while a == 1:
+    b = int(input("введите целое, натуральное число: "))
+    if b == 0:
+        print("Вы ввели число не соответствующее требованиям")
+        continue
+    elif b is int(b):
+        b = str(b)
+        calc(b)
+    a = int(input("хотите продолжить ? (1 - да, 0 - нет)"))
+    if a == 1:
+        continue
+    elif a == 0:
+        break
+
+print("работа программы завершена !")

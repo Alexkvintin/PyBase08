35d8fc3256d9faebbb9ee2dfad8c4040e2eb9a04
Slipchenko Alexey <55618990+Alexkvintin@users.noreply.github.com>
2019-10-07 18:00:17 +0300

    Update hw_4_1.py

--- lesson04/hw_4_1.py
+++ lesson04/hw_4_1.py
@@ -1,15 +1,20 @@
 text = []
+d = {}
 
 
 def text_stat(t):
+    for l in t:
+        l += 1
+        print(l)
     t = str(text)
-    print(t)
     print("количество символов списка = ", len(t))
     for i in t:
         if i == '.' or i == ';' or i == ']' or i == '[' or i == ' ' or i == '(' or i == ')' or i == '\'' or i == ',':
             pass
         else:
-            print(i, t.count(i))
+            d[i] = t.count(i)
+    for i in d:
+     print(i, d[i])
 
 
 b = input("введите дянные: ")
@@ -18,5 +23,22 @@ while True:
     b = input()
     if not b:
         break
+p = ['.', ',', ':', ';', '!', '?', '(', ')', '[', ']', '&', '*']
+print(text)
+i = 0
+for w in text:
+    if w[-1] in p:
+        text[i] = w[:-1]
+        w = text[i]
+    if w[0] in p:
+        text[i] = w[1:]
+        i += 1
+
+i = 0
+while i < len(text):
+    print(text[i], end=' ')
+    i += 1
+    if i % 1 == 0:
+     print()
 print(text)
 text_stat(text)

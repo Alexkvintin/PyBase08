ff89fb3cea6b89cd522db327458f374a710e27ba
Slipchenko Alexey <55618990+Alexkvintin@users.noreply.github.com>
2019-10-10 11:32:13 +0300

    Update hw_4_1.py

--- lesson04/hw_4_1.py
+++ lesson04/hw_4_1.py
@@ -3,13 +3,52 @@ d = {}
 
 
 def text_stat(t):
-    for l in t:
+    l = 0
+    w = 0
+    b = 0
+    g2 = ' ,:@.-()/'
+    g = t
+    for gi in g2:
+        Temp = []
+        n = len(g)
+        for i in range(n):
+            h = g[i].split(str(gi))
+            while True :
+                try :
+                    h.remove('')
+                except ValueError:
+                    break
+            Temp = Temp + h
+            if i == n - 1 :
+                g = Temp[:]
+    print(g)
+    while True:
+        try:
+            print(g.pop())
+        except:
+            break
+    for i in g:
+       x = g.pop()
+       print(x)
+    for i in text:
         l += 1
-        print(l)
+        b += len(i)
+        counter = True
+        for k in i:
+            if k != ' ' and counter == True:
+                w += 1
+                counter = False
+            elif k == ' ':
+                counter = True
+
+    print("количество строк в тексте: ", l)
+    print("Количестро слов в тексте: ", w)
+    print("количество символов в тексте: ", b)
+
     t = str(text)
-    print("количество символов списка = ", len(t))
     for i in t:
-        if i == '.' or i == ';' or i == ']' or i == '[' or i == ' ' or i == '(' or i == ')' or i == '\'' or i == ',':
+        if i == '@' or i == '.' or i == ';' or i == ']' or i == '[' or i == ' ' or i == '(' or i == ')' or i == '\'' \
+                or i == ',':
             pass
         else:
             d[i] = t.count(i)
@@ -17,28 +56,13 @@ def text_stat(t):
      print(i, d[i])
 
 
-b = input("введите дянные: ")
+a = input("введите дянные: ")
 while True:
-    text.append(b)
-    b = input()
-    if not b:
+    text.append(a)
+    a = input()
+    if not a:
         break
-p = ['.', ',', ':', ';', '!', '?', '(', ')', '[', ']', '&', '*']
-print(text)
-i = 0
-for w in text:
-    if w[-1] in p:
-        text[i] = w[:-1]
-        w = text[i]
-    if w[0] in p:
-        text[i] = w[1:]
-        i += 1
 
-i = 0
-while i < len(text):
-    print(text[i], end=' ')
-    i += 1
-    if i % 1 == 0:
-     print()
+p = ['.', ',', ':', ';', '!', '?', '(', ')', '[', ']', '&', '*']
 print(text)
 text_stat(text)

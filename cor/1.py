ac7b54ffe8e41a622d79ce44b511c44eb808a7ee
Slipchenko Alexey <55618990+Alexkvintin@users.noreply.github.com>
2019-10-16 19:45:55 +0300

    Update 1.py

--- cor/1.py
+++ cor/1.py
@@ -2,11 +2,13 @@ class Person:
     n = 0
     name = None
     surname = None
-    patronymic = None
-    mail = None
-    phone = None
-    address = None
-    messenger = None
+    def __init__(self, **kwargs):
+        patronymic = None
+        mail = None
+        phone = None
+        address = None
+        messenger = None
+
 
 
 try:
@@ -28,7 +30,7 @@ while True:
         continue
     if a == 1:
         g = {}
-        j = {}
+        j = []
         f = open('t.txt', 'a')
         Person.n += 1
         Person.name = input('Введите имя: ')
@@ -36,14 +38,45 @@ while True:
         b = input("хотите ввести еще данные (1 - да, 0 - нет)")
         if b == "1":
             Person.patronymic = input('Введите отчество: ')
+            phones = []
+            mails = []
             Person.mail = input('Введите основную почту: ')
+            mails.append(Person.mail)
+            while True:
+             c = input("that's all ? (1 - yes, ENTER - continue)")
+             if c == "1":
+              break
+             else:
+              Person.mail = input('Введите дополнительную почту: ')
+              mails.append(Person.mail)
+              continue
+            print(mails)
             Person.phone = input('Введите основной номер телефона: ')
+            phones.append(Person.phone)
+            while True:
+             c = input("that's all ? (1 - yes, ENTER - continue)")
+             if c == "1":
+              break
+             else:
+              Person.phone = input('Введите дополнительный номер телефона: ')
+              phones.append(Person.phone)
+              continue
+
+            print(phones)
             Person.address = input('Введите адрес: ')
-            g.a
         else:
             pass
-        v = f'Person {Person.n}\n Name: {Person.name}, Surname: {Person.surname}, ' \
-            f'Patronymic: {Person.patronymic}, Mail: {Person.mail}, Phone: {Person.phone} \n '
+        if b == "1":
+            g = dict(**{'Name': Person.name, 'Surname': Person.surname, 'Patronymic': Person.patronymic, 'Mail': Person.mail, 'Phone': Person.phone})
+        else:
+            g = dict(**{'Name': Person.name, 'Surname': Person.surname })
+        j.append(g)
+        print(j)
+        if b == "1":
+            v = f'Person {Person.n}\nName: <<{Person.name}>> Surname: <<{Person.surname}>> ' \
+                f'Patronymic: <<{Person.patronymic}>> Mail: <<{Person.mail}>>, Phone: <<{Person.phone}>> \n '
+        else:
+            v = f'Person {Person.n}\nName: <<{Person.name}>> Surname: <<{Person.surname}>> '
         f.write(v)
         f.close()
     elif a == 2:

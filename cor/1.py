f0fc903b7b650645f4ba076af036f10a29accfbf
Slipchenko Alexey <55618990+Alexkvintin@users.noreply.github.com>
2019-10-18 14:21:29 +0300

    Update 1.py

--- cor/1.py
+++ cor/1.py
@@ -1,94 +1,145 @@
-class Person:
+class Person :
     n = 0
     name = None
     surname = None
-    def __init__(self, **kwargs):
-        patronymic = None
-        mail = None
-        phone = None
-        address = None
-        messenger = None
 
 
+def _save(x):
+    h = open('b.txt', 'a')
+    for i in x[:] :
+        h.write(repr(x.pop()))
+        h.write('\n')
+    h.close()
+
+
+def write(x):
+    import pickle
+    t = x.pop(-1)
+    v = t
+    print(t)
+    print(v)
+    f = open('t.txt', 'ab')
+    pickle.dump(v, f)
+    f.close()
+
+
+def newfile():
+    from os import rename
+    f = open('newfile.txt', 'w')
+    f.close()
+
+
+def _open():
+    import pickle
+    f = open('t.txt', 'rb')
+    a = pickle.load(f)
+    print(a)
+    f.close()
 
 try:
-    f = open('t.txt')
+    f = open('t.txt', 'ar')
 except:
     f = open('t.txt', 'w')
 
-v = {}
+try:
+    h = open('b.txt', 'ar')
+except :
+    h = open('b.txt', 'w')
 
-while True:
-    try:
-     a = int(input("""
+o = []
+v = {}
+j = []
+while True :
+    try :
+        a = int(input("""
               Главное меню
     создать новый контакт нажмите: 1
     посмотреть список контактов нажмите: 2
-    закончить работу программы нажмите: 3
+    записать данные в файл нажмите 3
+    посмотреть список контактов нажмите: 4
+    закончить работу программы нажмите: 5
     """))
-    except:
+    except :
         continue
-    if a == 1:
+    if a == 1 :
         g = {}
-        j = []
         f = open('t.txt', 'a')
-        Person.n += 1
-        Person.name = input('Введите имя: ')
-        Person.surname = input('Введите фамилию: ')
+        Pn = Person
+        Pn.n += 1
+        Pn.name = input('Введите имя: ')
+        Pn.surname = input('Введите фамилию: ')
         b = input("хотите ввести еще данные (1 - да, 0 - нет)")
-        if b == "1":
-            Person.patronymic = input('Введите отчество: ')
-            phones = []
+        if b == "1" :
             mails = []
-            Person.mail = input('Введите основную почту: ')
-            mails.append(Person.mail)
-            while True:
-             c = input("that's all ? (1 - yes, ENTER - continue)")
-             if c == "1":
-              break
-             else:
-              Person.mail = input('Введите дополнительную почту: ')
-              mails.append(Person.mail)
-              continue
-            print(mails)
-            Person.phone = input('Введите основной номер телефона: ')
-            phones.append(Person.phone)
-            while True:
-             c = input("that's all ? (1 - yes, ENTER - continue)")
-             if c == "1":
-              break
-             else:
-              Person.phone = input('Введите дополнительный номер телефона: ')
-              phones.append(Person.phone)
-              continue
+            phones = []
+            Pn.patronymic = input('Введите отчество: ')
+            Pn.mail = input('Введите основную почту: ')
+            if not Pn.mail:
+                pass
+            else:
+             mails.append(Pn.mail)
+             while True :
+                c = input("that's all ? (1 - yes, ENTER - continue)")
+                if c == "1" :
+                    break
+                else :
+                    Pn.mail = input('Введите дополнительную почту: ')
+                    mails.append(Pn.mail)
+                    continue
 
+            Pn.phone = input('Введите основной номер телефона: ')
+            if not Pn.phone:
+                pass
+            else:
+             phones.append(Pn.phone)
+             while True :
+                c = input("that's all ? (1 - yes, ENTER - continue)")
+                if c == "1" :
+                    break
+                else :
+                    Pn.phone = input('Введите дополнительный номер телефона: ')
+                    phones.append(Pn.phone)
+                    continue
             print(phones)
-            Person.address = input('Введите адрес: ')
-        else:
+            print(mails)
+            Pn.address = input('Введите адрес: ')
+        else :
             pass
-        if b == "1":
-            g = dict(**{'Name': Person.name, 'Surname': Person.surname, 'Patronymic': Person.patronymic, 'Mail': Person.mail, 'Phone': Person.phone})
-        else:
-            g = dict(**{'Name': Person.name, 'Surname': Person.surname })
+        if b == "1" :
+            g = dict(
+                {'Name' : Pn.name, 'Surname' : Pn.surname, 'Patronymic' : Pn.patronymic, 'Mail' : mails,
+                 'Phone' : phones, 'Address' : Pn.address})
+        else :
+            g = dict({'Name' : Pn.name, 'Surname' : Pn.surname})
         j.append(g)
+        if b == "1" :
+            l = f'Person {Pn.n}\nName: <<{Pn.name}>> Surname: <<{Pn.surname}>> ' \
+                f'Patronymic: <<{Pn.patronymic}>> Mail: <<{Pn.mail}>>, Phone: <<{Pn.phone}>> \n '
+        else :
+            l = f'Person {Person.n}\nName: <<{Person.name}>> Surname: <<{Person.surname}>> '
+        o.append(g)
+        print(o)
+        print(g)
         print(j)
-        if b == "1":
-            v = f'Person {Person.n}\nName: <<{Person.name}>> Surname: <<{Person.surname}>> ' \
-                f'Patronymic: <<{Person.patronymic}>> Mail: <<{Person.mail}>>, Phone: <<{Person.phone}>> \n '
-        else:
-            v = f'Person {Person.n}\nName: <<{Person.name}>> Surname: <<{Person.surname}>> '
-        f.write(v)
-        f.close()
-    elif a == 2:
-        f = open('t.txt')
-        for i in f:
+        phones = []
+        mails = []
+
+    elif a == 2 :
+        h = open('b.txt', 'r')
+        for i in h :
             print(i)
-        f.close()
-    elif a == 3:
+        h.close()
+    elif a == 3 :
+        write(j)
+    elif a == 4:
+        _open()
+        del a
+    elif a == 5 :
         c = input('Сохранить данные? (1 - да 0 - нет)')
-        if c == '1':
-            f.write(v)
-        else:
+        if c == '1' :
+            _save(o)
+        else :
             pass
         break
 f.close()
+h.close()
